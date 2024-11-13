c95cd2294449b16252e20a0e9cfa29b86b8e6f32
Manjot Singh <48624649+manjotdhiman@users.noreply.github.com>
2024-11-13 16:38:39 +0530

    Added commit lint (#3)
    * feat: added lint test
    
    * feat: added ci
    
    * fix: ci
    
    * fix: lint fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,67 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import pluginReact from 'eslint-plugin-react';
-import testingLibrary from 'eslint-plugin-testing-library'; // Import testing-library plugin
+import testingLibrary from 'eslint-plugin-testing-library';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
+      parser: tsParser, // Use TypeScript parser for TypeScript files
       globals: globals.browser,
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginJs.configs.recommended, // Recommended JS rules from @eslint/js
 
-  // Override configuration for test files
+  // TypeScript configuration
   {
-    files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+    },
     plugins: {
-      'testing-library': testingLibrary,
+      '@typescript-eslint': tseslint,
     },
     rules: {
-      // Extend testing-library recommended rules
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+
+  // React configuration
+  {
+    files: ['**/*.{jsx,tsx}'],
+    plugins: { react: pluginReact },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+    },
+  },
+
+  // Jest and Node environments for test and config files
+  {
+    files: [
+      '**/__tests__/**/*.[jt]s?(x)',
+      '**/?(*.)+(spec|test).[jt]s?(x)',
+      'jest.setup.js',
+      'commitlint.config.js',
+      '__mocks__/**/*.ts',
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.jest, // Adds Jest globals like 'describe', 'it', 'jest'
+        ...globals.node, // Adds Node.js globals like 'module'
+      },
+    },
+    plugins: { 'testing-library': testingLibrary },
+    rules: {
       ...testingLibrary.configs.react.rules,
-      // Add any additional rules specific to test files
     },
   },
 ];
