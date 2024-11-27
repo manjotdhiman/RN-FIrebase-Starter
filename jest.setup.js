8e110deb82c5660913bec6d8b4dfb9dacc5e3c07
Manjot Singh <48624649+manjotdhiman@users.noreply.github.com>
2024-11-27 11:33:09 +0530

    fix: test

--- jest.setup.js
+++ jest.setup.js
@@ -2,22 +2,6 @@ import '@testing-library/react-native/extend-expect';
 
 import 'react-native-gesture-handler/jestSetup';
 
-jest.mock('react-native-gesture-handler', () => {
-  return {
-    PanGestureHandler: jest.fn().mockImplementation(({ children }) => children),
-    State: {},
-    TapGestureHandler: jest.fn().mockImplementation(({ children }) => children),
-  };
-});
-
-jest.mock('react-native-screens', () => {
-  return {
-    enableScreens: jest.fn(),
-    Screen: jest.fn(({ children }) => children),
-    ScreenContainer: jest.fn(({ children }) => children),
-  };
-});
-
 jest.mock('react-native-screens', () => {
   return {
     enableScreens: jest.fn(),
